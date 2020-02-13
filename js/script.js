e791355bf25cbe1b52548de93f00017c4c227fbe
Senad Redzic <redzicsenad90@gmail.com>
2020-02-13 21:23:12 +0100

    Adding quotes array

---
+++ js/script.js
@@ -0,0 +1,58 @@
+/******************************************
+Treehouse FSJS Techdegree:
+project 1 - A Random Quote Generator
+******************************************/
+
+// For assistance: 
+  // Check the "Project Resources" section of the project instructions
+  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
+
+/*** 
+ * `quotes` array 
+***/
+
+var quotes = [
+  {
+    quote: "",
+    source: ""
+  }, 
+  {
+    quote: "",
+    source: "",
+    year:""
+  }, 
+  {
+    quote: "",
+    source: "",
+    citation:""
+  }, 
+  {
+    quote: "",
+    source: ""
+  }, 
+  {
+    quote: "",
+    source: ""
+  }
+]; 
+
+console.log(quotes);
+
+/***
+ * `getRandomQuote` function
+***/
+
+
+
+/***
+ * `printQuote` function
+***/
+
+
+
+/***
+ * click event listener for the print quote button
+ * DO NOT CHANGE THE CODE BELOW!!
+***/
+
+document.getElementById('load-quote').addEventListener("click", printQuote, false);
